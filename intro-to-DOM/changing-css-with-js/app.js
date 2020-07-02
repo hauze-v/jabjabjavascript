09018c360d2afa48a015c03cb5f55c5e7dd06cde
Adam Hauze <hauze_v@protonmail.com>
2020-07-02 10:21:24 -0400

    Changing CSS with pure JS is easy using querySelector and the built-in .style method

---
+++ intro-to-DOM/changing-css-with-js/app.js
@@ -0,0 +1,20 @@
+// Whenever you're grabbing a reference to an HTML element, you want to grab it at the top of the file
+
+const bodyEl = document.querySelector('body')
+const divEl = document.querySelector('div')
+
+// Body styles
+bodyEl.style.backgroundColor = "#f0f0f2";
+bodyEl.style.margin = "0";
+bodyEl.style.padding = "0";
+bodyEl.style.fontFamily = "Open Sans";
+
+// div styles
+divEl.style.width = "600px";
+divEl.style.margin = "5em auto";
+divEl.style.padding = "2em";
+divEl.style.backgroundColor = "#fdfdff";
+divEl.style.borderRadius = "0.5em";
+divEl.style.boxShadow = "2px 3px 7px 2px rgba(0, 0, 0, 0.02)";
+
+
