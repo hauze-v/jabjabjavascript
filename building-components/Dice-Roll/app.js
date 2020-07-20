41513267bbc6c85882d5e6940888dd13a0853dd0
hauze-v <hauze_v@protonmail.com>
2020-07-20 18:21:32 -0400

    Finished Dice Roller app all on my own, from scratch, without any tutorials beforehand!

--- building-components/Dice-Roll/app.js
+++ building-components/Dice-Roll/app.js
@@ -1,2 +1,17 @@
-const x = document.querySelector()
+// Define references to the button and image result element.
+const triggerButtonEl = document.querySelector(".dice__trigger")
+const imageResultEl = document.querySelector(".dice__result")
 
+function diceGame() {
+  // Event listener for clicking button that randomly displays one of the 6 dice images
+  triggerButtonEl.addEventListener("click", function() {
+    imageResultEl.src = `images/dice${random()}.png`
+  })
+}
+
+// Seperate function for grabbing a random number between 1 and 6
+function random() {
+  return Math.floor(Math.random() * 6 + 1);
+}
+
+diceGame()
