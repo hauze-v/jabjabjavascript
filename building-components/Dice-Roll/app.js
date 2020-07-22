36c2bfd35476dfcfeb2c4c7439e466ce03173f06
hauze-v <hauze_v@protonmail.com>
2020-07-22 09:18:41 -0400

    Added sound effect and animation to the image

--- building-components/Dice-Roll/app.js
+++ building-components/Dice-Roll/app.js
@@ -3,9 +3,17 @@ const diceTriggerEl = document.querySelector(".dice__trigger")
 const diceImageEl = document.querySelector(".dice__image")
 
 function diceGame() {
+  // Declare an audio object and assign the dice.mp3 sound
+  let diceAudio = new Audio("audio/dice.mp3")
+
   // Event listener for clicking button that randomly displays one of the 6 dice images
   diceTriggerEl.addEventListener("click", function() {
+    // Animate the image
+    diceImageEl.classList.add("spin")
     diceImageEl.src = `images/dice${getRandomNumber()}.png`
+
+    // Play dice.mp3
+    diceAudio.play()
   })
 }
 
