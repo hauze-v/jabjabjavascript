679f5aa1a0e0a9c5c32662b9c35bd3b6620f96fe
hauze-v <hauze_v@protonmail.com>
2020-08-04 09:13:00 -0400

    Added 4 buttons for 30%, 50%, 75% and refresh

--- building-components/Youtube-Loading-Bar/app.js
+++ building-components/Youtube-Loading-Bar/app.js
@@ -1,5 +1,6 @@
 // DOM references
 const loadingBarItemEl = document.querySelector(".loading-bar__item")
+const loadingBarButtonEls = document.querySelectorAll(".loading-bar__button")
 
 let width = 0;
 let loadingBar = setInterval(function() { // Set interval is a built-in method in JS given to us by the browser
@@ -10,3 +11,22 @@ let loadingBar = setInterval(function() { // Set interval is a built-in method i
     loadingBarItemEl.style.width = `${width}%`;
   }
 }, 8)
+
+// Loop through all the buttons and add eventListeners
+for (let button of loadingBarButtonEls) {
+  button.addEventListener("click", function(event) {
+    console.log(event.target.textContent)
+    if (event.target.textContent == "30%") {
+      loadingBarItemEl.style.width = "30%"
+    }
+    if (event.target.textContent == "50%") {
+      loadingBarItemEl.style.width = "50%"
+    }
+    if (event.target.textContent == "75%") {
+      loadingBarItemEl.style.width = "75%"
+    }
+    if (event.target.textContent == "Refresh") {
+      window.location.reload();
+    }
+  })
+}
