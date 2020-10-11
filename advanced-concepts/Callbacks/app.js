d66b82048d6830f2ae76f25d0728c6a5e9a75640
Adam Hauze <hauze_v@protonmail.com>
2020-10-11 11:32:59 -0400

    Finished learning about callbacks in JS. It's also called functional programming

--- advanced-concepts/Callbacks/app.js
+++ advanced-concepts/Callbacks/app.js
@@ -7,18 +7,18 @@ function second(alias, callback) {
   callback(alias);
 }
 
-second("John", first);
+// second("John", first);
 
 /* A callback function is a function that is passed into another function as an argument and 
 this passed in function is invoked in the body of the other function */
 
-/* Common example of callback function is the addEventListener function */
+/* Common example of callback function is the addEventListener function 
 document.addEventListener("click", function(event) {
   // This is the inside of a callback function
-})
+}) */
 
-/* Example 1 */
-const greeter = (name, language) => {
+/* --- Example 1 --- */
+const greeter1 = (name, language) => {
   if (language === "English") {
     console.log(`Hello, ${name}!`);
   } else if (language === "Spanish") {
@@ -29,3 +29,130 @@ const greeter = (name, language) => {
     console.log("Idk");
   }
 }
+
+greeter1("Adam", "English"); // without callbacks
+
+/* With callbacks */
+const greeter2 = (name, callback) => {
+  callback(name);
+}
+
+const spanish = (name) => {
+  console.log(`Hola, ${name}`);
+}
+const french = (name) => {
+  console.log(`Bonjour, ${name}`);
+}
+const longJapanese = (name) => {
+  console.log(`Konichiiiiiiiiwa, ${name}!!!!!!!`);
+}
+
+greeter2("John", function(alias) {
+  console.log(`Hello, ${alias}`)
+});
+
+greeter2("Adam", function(alias) {
+  console.log(`Knichiwa, ${alias}`)
+})
+
+greeter2("Dustin", spanish);
+greeter2("Herman", french);
+greeter2("Legoshi", longJapanese);
+
+
+/* --- Example 2 --- */
+const squareZapper = (arr) => {
+  let result = [];
+  for (let item of arr) {
+    result.push(Math.pow(item, 2));
+  }
+  return result;
+}
+
+const doubleZap = (arr) => {
+  let result = [];
+  for (let item of arr) {
+    result.push(item + item);
+  }
+  return result;
+}
+
+const squareRootZap = (arr) => {
+  let result = [];
+  for (let item of arr) {
+    result.push(Math.sqrt(item));
+  }
+  return result;
+}
+
+const upperCaseZap = (arr) => {
+  let result = [];
+  for (let item of arr) {
+    result.push(item.toUpperCase())
+  }
+  return result;
+}
+
+const masterZap = (arr, callback) => {
+  let result = [];
+  for (let item of arr) {
+    result.push(callback(item));
+  }
+  return result;
+}
+
+const uppercase = (item) => item.toUpperCase();
+const double = (item) => item + item;
+
+/* Without callbacks */
+console.log(squareZapper([1,2,3]));
+console.log(doubleZap([1,2,3]));
+console.log(squareRootZap([4, 9, 64]));
+console.log(upperCaseZap(["apple", "mango", "banana"]));
+
+/* With callbacks */
+console.log(masterZap([1,2,3], function(item) {
+  return item * 2;
+}))
+console.log(masterZap([1,2,3], function(item) {
+  return Math.pow(item, 2);
+}))
+console.log(masterZap(["a", "p", "p"], function(item) {
+  return item.toUpperCase();
+}))
+console.log(masterZap([2,4,6], double));
+console.log(masterZap(["adam", "is", "awesome"], uppercase))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
