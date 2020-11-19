ee48f37957862bf5ce8125760e88f08c9cc5f540
Adam Hauze <hauze_v@protonmail.com>
2020-11-19 10:58:18 -0500

    Learned and practiced some Asynchronous Callback examples

---
+++ asynchronous-callbacks/intro.js
@@ -0,0 +1,46 @@
+function eatRamen() {
+  /*
+    1. boil water - 10 mins
+    2. prepare onions - 3 mins
+    3. prepare eggs - 1 min
+    4. mix and eat - 5 mins
+  */
+  boilWater((elem) => {
+    console.log(elem);
+    prepareOnions((elem) => {
+      console.log(elem);
+      prepareEggs((elem) => {
+        console.log(elem);
+        mix((elem) => {
+          console.log(elem);
+        });
+      });
+    });
+  });
+};
+
+const boilWater = (callback) => {
+  setTimeout(_ => {
+    callback("water ready");
+  }, 4000);
+};
+
+const prepareOnions = (callback) => {
+  setTimeout(_ => {
+    callback("onions ready");
+  }, 3000);
+};
+
+const prepareEggs = (callback) => {
+  setTimeout(_ => {
+    callback("eggs ready");
+  }, 1000);
+};
+
+const mix = (callback) => {
+  setTimeout(_ => {
+    callback("mixed and ready to eat");
+  }, 6000);
+};
+
+eatRamen();
